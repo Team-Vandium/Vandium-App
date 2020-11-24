86649fd50b5ebffc0d5ad23682b9a4e415788ef4
Conor Walsh <conorwalsh0703@gmail.com>
2020-11-24 14:01:01 +0000

    changed app.js to a class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
+import React, { Component } from 'react';
 import ProductList from './ProductList.js';
 import './App.css';
 import 'bootswatch/dist/yeti/bootstrap.min.css';
 import FilterBox from './Components/FilterBox.js';
 import ProductCard from './Components/ProductCard.js';
 
-function App() {
-  return (
-    <div className="App">
-      <ProductList />
-      <div className="container-md">
-        {/* Started on Filter box with checkboxs, can be integrated with search box */}
-        <FilterBox></FilterBox>
-        <div className="row">
-          <div className="col-4">
-            {/* Sample Product Card - to be added to */}
-            <ProductCard></ProductCard>
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // get product data from api and store in state
+    this.state = {
+      apiData: [],
+      isFetched: false,
+      errorMsg: null,
+    };
+  }
+
+  async componentDidMount() {
+    try {
+      const API_URL =
+        "https://raw.githubusercontent.com/Team-Vandium/data/main/products-jaymie.json";
+      // fetch data from api
+      const response = await fetch(API_URL);
+      // store response
+      const jsonResult = await response.json();
+
+      this.setState({ apiData: jsonResult.products });
+      this.setState({ isFetched: true });
+    } catch (error) {
+      // API threw an error
+      this.setState({ isFetched: false });
+      // save to variable to display the error
+      this.setState({ errorMsg: error });
+    } // end of try catch
+  } // end of componentDidMount()
+
+  render() {
+    return (
+      <div className="App">
+        <ProductList />
+        <div className="container-md">
+          {/* Started on Filter box with checkboxs, can be integrated with search box */}
+          <FilterBox></FilterBox>
+          <div className="row">
+            <div className="col-4">
+              {/* Sample Product Card - to be added to */}
+              <ProductCard></ProductCard>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
