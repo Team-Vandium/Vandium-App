442baeed80d464e3d499df59604329e28b49413a
jaymie-ob <jaymie.obrien@gmail.com>
2020-12-04 10:52:48 +0000

    newsletter

--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -14,6 +14,7 @@ class Newsletter extends Component {
   validateEmail(email) {
     let result = false;
     //const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // const re = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9+_.-]+\.[a-zA-Z]{2,}$/
     if (/^\S+@\S+\.\S+$/.test(String(email).toLowerCase())) {
       result = true;
     }
