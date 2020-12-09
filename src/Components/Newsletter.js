2b653a1c9674b03cfb2a4837a382fe4506ce60ca
jaymie-ob <jaymie.obrien@gmail.com>
2020-12-09 10:24:16 +0000

    tidy up newsletter page.

--- src/Components/Newsletter.js
+++ src/Components/Newsletter.js
@@ -2,12 +2,12 @@ import React, { Component } from "react";
 
 class Newsletter extends Component {
   /*
-  * A newsletter page, where a visitor can sign up for a weekly newsletter.
-  * Nothing actually gets emailed to the person.
-  * Email address will be validated and checked against existing mailing list.
-  * If it's a new address it will be added to the mailing list. 
-  * 
-  */
+   * A newsletter page, where a visitor can sign up for a weekly newsletter.
+   * Nothing actually gets emailed to the person.
+   * Email address will be validated and checked against existing mailing list.
+   * If it's a new address it will be added to the mailing list.
+   *
+   */
   constructor(props) {
     super(props);
     this.state = {
@@ -27,15 +27,15 @@ class Newsletter extends Component {
   validateEmail(email) {
     let result = false;
     /*
-    * Trying various examples of regex to validate the address.
-    * Not perfect but does an okay job.
-    * from here: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-    */
+     * Trying various examples of regex to validate the address.
+     * Not perfect but does an okay job.
+     * from here: https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+     */
     //const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     //if (/^\S+@\S+\.\S+$/.test(String(email).toLowerCase())) {
-    
-    const re = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9+_.-]+\.[a-zA-Z]{2,}$/
-    
+
+    const re = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9+_.-]+\.[a-zA-Z]{2,}$/;
+
     if (re.test(String(email).toLowerCase())) {
       result = true;
     }
@@ -56,12 +56,12 @@ class Newsletter extends Component {
     );
     // found email address
     if (found !== undefined) {
-      console.log(found.address);
+      //console.log(found.address);
       this.setState({ emailExists: true });
     }
     // new email address
     else {
-      console.log("new email address");
+      //console.log("new email address");
       this.props.addItemToEmails(this.state.emailInput);
     }
 
@@ -105,22 +105,6 @@ class Newsletter extends Component {
                     </button>
                   </div>
                 </div>
-                <div className="emailResults">
-                  <table border="1">
-                    <thead>
-                      <th> ID </th>
-                      <th> ADDRESS </th>
-                    </thead>
-                    <tbody>
-                      {emails.map((e) => (
-                        <tr key={e.id}>
-                          <td>{e.id}</td>
-                          <td>{e.address}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                </div>
 
                 <br />
                 {!this.state.emailValid && (
@@ -135,6 +119,7 @@ class Newsletter extends Component {
             // Email submitted and was not already on list
             this.state.submitted && !this.state.emailExists && (
               <p>
+                <br />
                 Thank you! You will receive the weekly newsletter to{" "}
                 {this.state.emailInput}.
               </p>
@@ -143,7 +128,10 @@ class Newsletter extends Component {
           {
             // Email submitted but already on mailing list
             this.state.submitted && this.state.emailExists && (
-              <p>{this.state.emailInput} is already on our mailing list.</p>
+              <p>
+                <br />
+                {this.state.emailInput} is already on our mailing list.
+              </p>
             )
           }
         </div>
